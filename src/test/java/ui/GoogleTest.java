a361a4264588528216bd727e0584bf62a0703835
Stanislau Sindalouski <ssindalovskiy@alfabank.ru>
2025-04-26 23:29:35 +0300

    GoogleTest added

---
+++ src/test/java/ui/GoogleTest.java
@@ -0,0 +1,28 @@
+package ui;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.testng.Assert.assertTrue;
+
+public class GoogleTest {
+
+    @BeforeClass
+    public void setUp() {
+        // Если тест будет крутиться в CI — раскомментируешь это:
+        // Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void googlePageTitleShouldContainGoogle() {
+        open("https://www.google.com");
+        String title = Selenide.title();
+        assertTrue(title.contains("Google"), "Page title should contain 'Google'");
+    }
+}
+
