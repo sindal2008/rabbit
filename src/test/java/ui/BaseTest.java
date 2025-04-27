8d2d7412763e20db8216a42dcfaa360e5d64a434
Stanislau Sindalouski <ssindalovskiy@alfabank.ru>
2025-04-27 15:46:36 +0300

    browsers.json added

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -14,17 +14,5 @@ public abstract class BaseTest {
         Configuration.remote = "http://localhost:4444/wd/hub"; // URL до Selenoid
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-
-        ChromeOptions options = new ChromeOptions();
-        Map<String, Object> selenoidOptions = new HashMap<>();
-//        selenoidOptions.put("enableVideo", true);
-        options.setCapability("selenoid:options", selenoidOptions);
-
-        // Генерация уникальной директории для пользовательских данных
-//        String tempDir = System.getProperty("java.io.tmpdir");
-//        String userDataDir = tempDir + "/chrome-user-data-" + System.currentTimeMillis();
-//        options.addArguments("--user-data-dir=" + userDataDir);
-
-        Configuration.browserCapabilities = options;
     }
 }
