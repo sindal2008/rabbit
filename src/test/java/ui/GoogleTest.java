2e2e18db4f04b19a96a2165389d331ac9a8a6ee7
s.sindalouski <s.sindalouski@andersenlab.com>
2025-06-23 17:43:24 +0300

    Dockerfile updated

--- src/test/java/ui/GoogleTest.java
+++ src/test/java/ui/GoogleTest.java
@@ -1,17 +1,31 @@
 package ui;
 
 import com.automation.remarks.video.annotations.Video;
-import com.codeborne.selenide.Selenide;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.*;
 import static org.testng.Assert.assertTrue;
 
 public class GoogleTest extends BaseTest {
 
     @Test
     @Video
-    public void googlePageTitleShouldContainGoogle() throws InterruptedException {
-        Thread.sleep(3000);
+    public void shouldFailAndCreateRecordWithTestName() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        assert false;
+    }
+
+    @Test
+    @Video(name = "second_test")
+    public void videoShouldHaveNameSecondTest() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        assertTrue(false);
     }
 }
