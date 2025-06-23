0644d97eec3ce0f56df820d5d40c208705904a69
s.sindalouski <s.sindalouski@andersenlab.com>
2025-06-23 15:30:58 +0300

    Video records added

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -4,15 +4,16 @@ import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public abstract class BaseTest {
 
     @BeforeClass
     public void setUp() {
-        Configuration.remote = "http://localhost:4444/wd/hub"; // URL до Selenoid
+//        Configuration.remote = "http://localhost:4444/wd/hub"; // URL до Selenoid
         Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        Configuration.browserCapabilities = options;
+
     }
 }
