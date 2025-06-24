1fd4e6b121a101464f9862339c856babdd121e88
s.sindalouski <s.sindalouski@andersenlab.com>
2025-06-24 12:17:32 +0300

    Dockerfile updated

--- src/test/java/ui/GoogleTest.java
+++ src/test/java/ui/GoogleTest.java
@@ -1,17 +1,45 @@
 package ui;
 
-import com.codeborne.selenide.Selenide;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.*;
-import static org.testng.Assert.assertTrue;
+import java.io.IOException;
 
 public class GoogleTest extends BaseTest {
 
+    private static Process ffmpegProcess;
+
+    @BeforeMethod
+    public static void startRecording(String methodName) {
+        String videoName = "target/video/" + methodName + ".mp4";
+        ProcessBuilder builder = new ProcessBuilder(
+                "ffmpeg",
+                "-y",
+                "-f", "x11grab",
+                "-video_size", "1536x768",
+                "-i", System.getenv("DISPLAY"),
+                "-codec:v", "libx264",
+                "-preset", "ultrafast",
+                videoName
+        );
+        builder.redirectErrorStream(true);
+        try {
+            ffmpegProcess = builder.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
-    public void googlePageTitleShouldContainGoogle() {
-        open("https://www.google.com");
-        String title = Selenide.title();
-        assertTrue(title.contains("Google"), "Page title should contain 'Google'");
+    public void googlePageTitleShouldContainGoogle() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
+    @AfterMethod
+    public static void stopRecording() {
+        if (ffmpegProcess != null) {
+            ffmpegProcess.destroy();
+        }
     }
 }
