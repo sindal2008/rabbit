dff1d7ac963cf8aedbb16802fd79b88d41c2659c
s.sindalouski <s.sindalouski@andersenlab.com>
2025-06-24 15:29:55 +0300

    Dockerfile updated

--- src/test/java/ui/GoogleTest.java
+++ src/test/java/ui/GoogleTest.java
@@ -1,10 +1,15 @@
 package ui;
+
+import com.codeborne.selenide.Selenide;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.testng.Assert.assertTrue;
 
 public class GoogleTest extends BaseTest {
 
@@ -34,7 +39,9 @@ public class GoogleTest extends BaseTest {
 
     @Test
     public void googlePageTitleShouldContainGoogle() throws InterruptedException {
-        Thread.sleep(5000);
+        open("https://www.rambler.ru");
+        String title = Selenide.title();
+        assertTrue(title.contains("Рамблер"), "Page title should contain 'Rambler'");
     }
 
     @AfterMethod
