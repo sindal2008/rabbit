d4169e0af2d438c523f0bae7f28200a65e020221
s.sindalouski <s.sindalouski@andersenlab.com>
2025-06-24 16:01:52 +0300

    Dockerfile updated

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -4,16 +4,24 @@ import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 
+import java.util.Arrays;
+import java.util.UUID;
+
 public abstract class BaseTest {
+
+    private static String uniqueProfileDir = "/tmp/chrome-profile-" + UUID.randomUUID();
 
     @BeforeClass
     public void setUp() {
-//        Configuration.remote = "http://localhost:4444/wd/hub"; // URL до Selenoid
         Configuration.browser = "chrome";
-
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=" + uniqueProfileDir);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless=new");  // или "--headless" в старых версиях
         options.addArguments("--start-maximized");
+        options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
         Configuration.browserCapabilities = options;
-
     }
 }
+
